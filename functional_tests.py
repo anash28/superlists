dd1225ddfb777a013c624c4914d4c9844f8ff51e
anash28 <alfredzo.nash@gmail.com>
2016-01-31 18:19:16 -0500

    changes from master

--- functional_tests.py
+++ functional_tests.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 import unittest
 
 class NewVisitor(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser = webdriver.quit()
@@ -15,7 +16,6 @@ class NewVisitor(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
